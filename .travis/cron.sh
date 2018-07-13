32ff1fdefe01bb77822368a364f81eb7f1ef9b73
Luolc <luolc.witty@gmail.com>
2018-07-13 17:38:56 +0800

    test

--- .travis/cron.sh
+++ .travis/cron.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+git clone https://github.com/Luolc/trump-tracker.git --branch gh-pages --depth 1
+mv trump-tracker build
+rm -rf build/.git
 python3 core/core.py fetch_tweets $CONSUMER_KEY $CONSUMER_SECRET $ACCESS_TOKEN_KEY $ACCESS_TOKEN_SECRET
 python3 core/core.py generate
